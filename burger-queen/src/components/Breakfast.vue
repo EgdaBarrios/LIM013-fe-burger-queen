34551d196d3f342970fc67ea2d93088a9b1fcb62
Egda Barrios <egdabarrios@gmail.com>
2021-01-11 21:13:38 -0500

    breakfast and otherMeals menu buttons

--- burger-queen/src/components/Breakfast.vue
+++ burger-queen/src/components/Breakfast.vue
@@ -4,16 +4,55 @@
       <h3 class="title">DESAYUNO</h3>
     </div>
     <div class="container">
-      <div class="subtitle">
-        <h4>Sandwiches</h4>
-      </div>
-      <div class="sandwichList">
-        <ul v-for="bs in breakfastSandwiches" :key="bs.id">
-          <li>
-            <h5>{{ bs.description }}</h5>
-            <p>{{ bs.price }}</p>
-          </li>
-        </ul>
+      <div class="contentArea">
+        <div class="subtitle">
+          <h4><span class="notBold">Sandwiches</span></h4>
+        </div>
+        <div class="sandwichList">
+          <ul v-for="bs in breakfastSandwiches" :key="bs.id">
+            <li>
+              <button type="button" class="itemBtn">
+                <img class="iconImg" alt="Sandwich" src="../assets/iconSandwich.png">
+                <div class="textBtn">
+                  <h5>{{ bs.description }}</h5>
+                  <p>S/ {{ bs.price }}.00</p>
+                </div>
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div class="subtitle">
+          <h4><span class="notBold">Bebidas Calientes</span></h4>
+        </div>
+        <div class="drinkList">
+          <ul v-for="bhd in breakfastHotDrinks" :key="bhd.id">
+            <li>
+              <button type="button" class="itemBtn">
+                <img class="iconImg" alt="Sandwich" src="../assets/iconCup.png">
+                <div class="textBtn">
+                  <h5>{{ bhd.description }}</h5>
+                  <p>S/ {{ bhd.price }}.00</p>
+                </div>
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div class="subtitle">
+          <h4><span class="notBold">Bebidas Frías</span></h4>
+        </div>
+        <div class="drinkList">
+          <ul v-for="bcd in breakfastColdDrinks" :key="bcd.id">
+            <li>
+              <button type="button" class="itemBtn">
+                <img class="iconImg" alt="Sandwich" src="../assets/iconJuice.png">
+                <div class="textBtn">
+                  <h5>{{ bcd.description }}</h5>
+                  <p>S/ {{ bcd.price }}.00</p>
+                </div>
+              </button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
@@ -26,11 +65,15 @@ export default {
   data() {
     return {
       breakfastSandwiches: [],
+      breakfastHotDrinks: [],
+      breakfastColdDrinks: [],
     };
   },
 
   firestore: {
     breakfastSandwiches: db.collection('breakfastSandwiches'),
+    breakfastHotDrinks: db.collection('breakfastHotDrinks'),
+    breakfastColdDrinks: db.collection('breakfastColdDrinks'),
   },
 };
 </script>
@@ -57,5 +100,56 @@ export default {
   text-align: left;
   font-family: Stencil;
   color: #131313;
+}
+
+.container {
+  width: 55%;
+  height: 480px;
+  text-align: left;
+  margin-left: 20px;
+  box-sizing: border-box;
+  overflow-y: scroll;
+}
+
+.contentArea {
+  height: 490px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.notBold {
+  font-family: Freestyle Script;
+  font-weight:normal;
+  font-size: 22px;
+}
+
+.itemBtn {
+  width: 95%;
+  height: 60px;
+  margin-bottom: 10px;
+  background-color: transparent;
+  border: solid 2px white;
+}
+
+.iconImg {
+  width: 20%;
+  height: 50px;
+  padding: 0, 2px;
+  vertical-align: top;
+  display: inline-block;
+  margin: 0;
+  background-color: white;
+}
+
+.textBtn {
+  width: 78%;
+  line-height: 0;
+  text-align: center;
+  display: inline-block;
+  color: white;
 }
 </style>
--- burger-queen/src/components/OtherMeals.vue
+++ burger-queen/src/components/OtherMeals.vue
@@ -3,13 +3,79 @@
     <div class="header">
       <h3 class="title">RESTO DEL DIA</h3>
     </div>
-    I love hamburgers!
+    <div class="container">
+      <div class="contentArea">
+        <div class="subtitle">
+          <h4><span class="notBold">Hamburguesas</span></h4>
+        </div>
+        <div class="sandwichList">
+          <ul v-for="omh in otherMealsHamburgers" :key="omh.id">
+            <li>
+              <button type="button" class="itemBtn">
+                <img class="iconImg" alt="Sandwich" src="../assets/iconHamburger.png">
+                <div class="textBtn">
+                  <h5>{{ omh.description }}</h5>
+                  <p>S/ {{ omh.price }}.00</p>
+                </div>
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div class="subtitle">
+          <h4><span class="notBold">Acompañamientos</span></h4>
+        </div>
+        <div class="sandwichList">
+          <ul v-for="oms in otherMealsSides" :key="oms.id">
+            <li>
+              <button type="button" class="itemBtn">
+                <img class="iconImg" alt="Sandwich" src="../assets/iconFries.png">
+                <div class="textBtn">
+                  <h5>{{ oms.description }}</h5>
+                  <p>S/ {{ oms.price }}.00</p>
+                </div>
+              </button>
+            </li>
+          </ul>
+        </div>
+        <div class="subtitle">
+          <h4><span class="notBold">Bebidas</span></h4>
+        </div>
+        <div class="sandwichList">
+          <ul v-for="omd in otherMealsDrinks" :key="omd.id">
+            <li>
+              <button type="button" class="itemBtn">
+                <img class="iconImg" alt="Sandwich" src="../assets/iconBottle.png">
+                <div class="textBtn">
+                  <h5>{{ omd.description }}</h5>
+                  <p>S/ {{ omd.price }}.00</p>
+                </div>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { db } from '../db';
+
 export default {
   name: 'OtherMeals',
+  data() {
+    return {
+      otherMealsHamburgers: [],
+      otherMealsSides: [],
+      otherMealsDrinks: [],
+    };
+  },
+
+  firestore: {
+    otherMealsHamburgers: db.collection('otherMealsHamburgers'),
+    otherMealsSides: db.collection('otherMealsSides'),
+    otherMealsDrinks: db.collection('otherMealsDrinks'),
+  },
 };
 </script>
 
@@ -35,5 +101,56 @@ export default {
   text-align: left;
   font-family: Stencil;
   color: #131313;
+}
+
+.container {
+  width: 55%;
+  height: 480px;
+  text-align: left;
+  margin-left: 20px;
+  box-sizing: border-box;
+  overflow-y: scroll;
+}
+
+.contentArea {
+  height: 600px;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+.notBold {
+  font-family: Freestyle Script;
+  font-weight:normal;
+  font-size: 22px;
+}
+
+.itemBtn {
+  width: 95%;
+  height: 60px;
+  margin-bottom: 10px;
+  background-color: transparent;
+  border: solid 2px white;
+}
+
+.iconImg {
+  width: 20%;
+  height: 50px;
+  padding: 0, 2px;
+  vertical-align: top;
+  display: inline-block;
+  margin: 0;
+  background-color: white;
+}
+
+.textBtn {
+  width: 78%;
+  line-height: 0;
+  text-align: center;
+  display: inline-block;
+  color: white;
 }
 </style>
